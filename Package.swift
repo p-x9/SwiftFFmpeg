f35300f760529c4c2118b06af177447e42481ba2
p-x9 <chi.iosdev@gmail.com>
2021-03-20 03:03:11 +0900

    add dependencies

--- Package.swift
+++ Package.swift
@@ -4,29 +4,33 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftFFmpeg",
-  products: [
-    .library(
-      name: "SwiftFFmpeg",
-      targets: ["SwiftFFmpeg"]
-    )
-  ],
-  targets: [
-    .systemLibrary(
-      name: "CFFmpeg",
-      pkgConfig: "libavformat"
-    ),
-    .target(
-      name: "SwiftFFmpeg",
-      dependencies: ["CFFmpeg"]
-    ),
-    .target(
-      name: "SwiftFFmpegExamples",
-      dependencies: ["SwiftFFmpeg"]
-    ),
-    .testTarget(
-      name: "SwiftFFmpegTests",
-      dependencies: ["SwiftFFmpeg"]
-    ),
-  ]
+    name: "SwiftFFmpeg",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "SwiftFFmpeg",
+            targets: ["SwiftFFmpeg"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kewlbear/FFmpeg-iOS.git", from: "0.0.2"),
+    ],
+    targets: [
+        .systemLibrary(
+            name: "CFFmpeg",
+            pkgConfig: "libavformat"
+        ),
+        .target(
+            name: "SwiftFFmpeg",
+            dependencies: ["CFFmpeg"]
+        ),
+        .target(
+            name: "SwiftFFmpegExamples",
+            dependencies: ["SwiftFFmpeg"]
+        ),
+        .testTarget(
+            name: "SwiftFFmpegTests",
+            dependencies: ["SwiftFFmpeg"]
+        ),
+    ]
 )
