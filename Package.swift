7cedb2e715e8b69569586a4d9ae5f924a5cd3b95
p-x9 <chi.iosdev@gmail.com>
2021-03-20 17:17:39 +0900

    test

--- Package.swift
+++ Package.swift
@@ -1,32 +1,36 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "SwiftFFmpeg",
-  products: [
-    .library(
-      name: "SwiftFFmpeg",
-      targets: ["SwiftFFmpeg"]
-    )
-  ],
-  targets: [
-    .systemLibrary(
-      name: "CFFmpeg",
-      pkgConfig: "libavformat"
-    ),
-    .target(
-      name: "SwiftFFmpeg",
-      dependencies: ["CFFmpeg"]
-    ),
-    .target(
-      name: "SwiftFFmpegExamples",
-      dependencies: ["SwiftFFmpeg"]
-    ),
-    .testTarget(
-      name: "SwiftFFmpegTests",
-      dependencies: ["SwiftFFmpeg"]
-    ),
-  ]
+    name: "SwiftFFmpeg",
+    platforms: [.iOS(.v9), .tvOS(.v10),.macOS(.v10_10)],
+    products: [
+        .library(
+            name: "SwiftFFmpeg",
+            targets: ["SwiftFFmpeg"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kewlbear/FFmpeg-iOS.git", from: "0.0.2"),
+    ],
+    targets: [
+        .systemLibrary(
+            name: "CFFmpeg",
+            pkgConfig: "libavformat"
+        ),
+        .target(
+            name: "SwiftFFmpeg",
+            dependencies: ["CFFmpeg"]
+        ),
+        .target(
+            name: "SwiftFFmpegExamples",
+            dependencies: ["SwiftFFmpeg"]
+        ),
+        .testTarget(
+            name: "SwiftFFmpegTests",
+            dependencies: ["SwiftFFmpeg"]
+        ),
+    ]
 )
